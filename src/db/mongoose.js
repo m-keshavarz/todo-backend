8a63a918c22655668cc735098ff47bcee4a80e82
mohamad keshavarz <m.k.dev@outlook.com>
2023-12-03 18:09:09 +0330

    fixed mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/task-manager-api",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
