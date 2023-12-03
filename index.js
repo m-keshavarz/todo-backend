26be4e25abfafb5b8f1f5926e0bb75eeef1ba353
mohamad keshavarz <m.k.dev@outlook.com>
2023-12-03 17:50:58 +0330

    different path for index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-require("./db/mongoose");
+require("./src/db/mongoose");
 
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
+const userRouter = require("./src/routers/user");
+const taskRouter = require("./src/routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
