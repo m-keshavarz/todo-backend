60afbdddf3570676e107a82330a77b7826e18b5e
mohamad keshavarz <m.k.dev@outlook.com>
2023-12-04 16:57:33 +0330

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require('cors')
 
 require("./src/db/mongoose");
+
 
 const userRouter = require("./src/routers/user");
 const taskRouter = require("./src/routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
